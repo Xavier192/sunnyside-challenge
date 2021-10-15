2acfdfe1d31b41e107b8da24091c6d53b0c5695c
Xavier <xavier01242@gmail.com>
2021-10-15 06:58:45 +0200

    Navbar menu arrow done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-        1 archivo(s) copiado(s).
+const navbarBehavior = function (){
+    const navbar = document.querySelector('.navbar');
+    const navbarToggler = document.querySelector('.navbar__toggler');
+    const navbarMenu = document.querySelector('.navbar__menu');
+
+    window.onscroll = function(){
+        if(window.pageYOffset > 0){
+            navbar.classList.add('navbar__scroll');
+        }
+        else{
+            navbar.classList.remove('navbar__scroll');
+        }
+    }
+
+    navbarToggler.onclick = function(){
+        navbarMenu.classList.toggle('navbar__toggle');
+    }
+
+
+    return{
+        navbar:navbar
+    }
+}();
