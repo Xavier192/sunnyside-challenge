079b366b2d48f6b068b4641a067466f62133beaa
Xavier <xavier01242@gmail.com>
2021-10-17 16:40:30 +0200

    Cleaned code solved errors

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const navbarBehavior = function (){
     const navbar = document.querySelector('.navbar');
     const navbarToggler = document.querySelector('.navbar__toggler');
     const navbarMenu = document.querySelector('.navbar__menu');
+    let animationStopTimer = null;
 
     window.onscroll = function(){
         if(window.pageYOffset > 0){
@@ -12,8 +13,22 @@ const navbarBehavior = function (){
         }
     }
 
+    window.onresize = function(){
+        document.body.id = 'stop-animations';
+        animationStopTimer = setTimeout(function(){
+            document.body.id = '';
+        },100);
+    }
+
     navbarToggler.onclick = function(){
         navbarMenu.classList.toggle('navbar__toggle');
+        navbarToggler.setAttribute('aria-expanded',getContraryAriaExpanded());
+    }
+
+    function getContraryAriaExpanded(){
+        let ariaExpanded = navbarToggler.getAttribute('aria-expanded');
+
+        return ariaExpanded === 'true' ? 'false' : 'true';
     }
 
 
